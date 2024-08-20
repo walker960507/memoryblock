61a47e292ec10573a18400bb13df0139d3413acb
walker960507 <walker960507@outlook.com>
2024-08-20 10:12:37 +0800

    Create core_param.v

---
+++ inc/core_param.v
@@ -0,0 +1,38 @@
+`define CORE_PARAM #( \
+             parameter L1D_BANK_SIZE = 16,\
+             parameter L1D_CACHELINE_SIZE = 64,\
+             parameter PA_WIDTH     = 32,\
+             parameter L1D_SIZE           = 32768,\
+             parameter L1D_NUM_WAYS       = 2,\
+             parameter L1D_TAG_STATE_WIDTH = 3,\
+             parameter STB_NUM             = 1,\
+             parameter REFILL_NUM          = 1,\
+             parameter SNPSLV_NUM          = 1,\
+             parameter INSTRUCTION_WIDTH   = 32,\
+             parameter PARAM_END = 0)
+
+`define REFILL_SLOT_PARAM #( \
+             parameter ID            = 0,\
+             parameter L1D_BANK_SIZE = 16,\
+             parameter L1D_CACHELINE_SIZE = 64,\
+             parameter PA_WIDTH           = 32,\
+             parameter L1D_SIZE           = 32768,\
+             parameter L1D_NUM_WAYS       = 2,\
+             parameter L1D_TAG_STATE_WIDTH = 3,\
+             parameter STB_NUM             = 1,\
+             parameter REFILL_NUM          = 1,\
+             parameter SNPSLV_NUM          = 1,\
+             parameter INSTRUCTION_WIDTH   = 32,\
+             parameter PARAM_END = 0)
+
+ `define REFILL_SLOT_PARAM_INST  .L1D_BANK_SIZE(L1D_BANK_SIZE),\
+                                 .L1D_CACHELINE_SIZE(L1D_CACHELINE_SIZE),\
+                                 .PA_WIDTH(PA_WIDTH),\
+                                 .L1D_SIZE(L1D_SIZE),\
+                                 .L1D_NUM_WAYS(L1D_NUM_WAYS),\
+                                 .L1D_TAG_STATE_WIDTH(L1D_TAG_STATE_WIDTH),\
+                                 .STB_NUM(STB_NUM),\
+                                 .REFILL_NUM(REFILL_NUM),\
+                                 .SNPSLV_NUM(SNPSLV_NUM),\
+                                 .INSTRUCTION_WIDTH(INSTRUCTION_WIDTH),\
+                                 .PARAM_END(1)
